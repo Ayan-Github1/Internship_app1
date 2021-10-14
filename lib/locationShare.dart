cea88eddc513fcf967d648c4484e980510771f78
nagaraj <nagarajkulkarni290@gmail.com>
2021-10-14 15:05:31 +0530

    Nagaraj's commit

--- lib/locationShare.dart
+++ lib/locationShare.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
+
 void main() => runApp(const LocationShare());
 
 class LocationShare extends StatelessWidget{
@@ -19,10 +20,244 @@ class LocationShareState extends StatefulWidget{
   State<LocationShareState> createState() => LocationPage();
 }
 
-class LocationPage extends State<LocationShareState>{
+class LocationPage extends State<LocationShareState> with SingleTickerProviderStateMixin{
+
+  List<String> titles = ["Location share"];
+  bool switchButton = true;
+
+  late TabController tabController;
+  late String title;
+
+  void initState(){
+    title = titles[0];
+    tabController = TabController(length: 2,vsync: this);
+    tabController.addListener(changeTitle);
+    super.initState();
+  }
+
+  void changeTitle(){
+    setState(() {
+      title = titles[tabController.index];
+    });
+  }
+
+  void change(bool value){
+    if(switchButton == true)
+    {
+      setState(() {
+        switchButton = false;
+      });
+    }
+    else
+    {
+      setState(() {
+        switchButton = true;
+      });
+    }
+  }
+
+  @override
   Widget build(BuildContext context){
     return Scaffold(
-      body: Center(child: Text('Location Page')),
+      appBar: AppBar(
+        iconTheme: IconThemeData(color: Colors.black),
+        leading: Padding(
+            padding: const EdgeInsets.all(1.0),
+            child: Builder(
+              builder: (context) =>
+                  IconButton(
+                    icon: Icon(Icons.arrow_back,size: 30.0,),
+                    onPressed: () => Scaffold.of(context).openDrawer(),
+                  ),
+            )
+        ),
+        elevation: 1.0,
+        backgroundColor: Colors.grey.shade200,
+        title: Text(title,style: TextStyle(color: Colors.black,fontWeight: FontWeight.bold,fontSize: 19.0),),
+
+          actions: [
+         title == "Location share" ?
+         Padding(
+          padding: const EdgeInsets.all(14.0),
+
+       )
+
+        : title == "" ?
+        Padding(
+        padding: const EdgeInsets.all(8.0),
+         child: Switch(
+        onChanged: change,
+        value: switchButton,
+        activeTrackColor: Colors.black26,
+        )
+        )
+            : Container(),
+       ],
+      ),
+
+      body: TabBarView(
+        controller: tabController,
+        children: <Widget>[
+          Sharedbyme(),
+          Sharedwithme(),
+        ],
+      ),
+
+        bottomNavigationBar: Container(
+          decoration: BoxDecoration(
+            border: Border(top: BorderSide(color: Colors.grey.shade300)),
+          ),
+          child: TabBar(
+            controller: tabController,
+            labelColor: Colors.blue,
+            unselectedLabelColor: Colors.grey.shade400,
+            tabs: [
+              Tab(icon: Icon(Icons.location_on_sharp,size: 20.0,),),// Didn't found the filled bar icon, replaced with other icon
+              Tab(icon: Icon(Icons.location_on_rounded,size: 20.0,)),
+              // Tab(icon: FaIcon(FontAwesomeIcons.bell),),
+            ],
+          ),
+        )
+
     );
   }
+}
+
+class Sharedbyme extends StatefulWidget{
+  const Sharedbyme({Key? key}) : super(key: key);
+
+  @override
+  State<StatefulWidget> createState() {
+    return SharedbymeState();
+  }
+}
+
+class SharedbymeState extends State<Sharedbyme>{
+
+  bool switchButton = true;
+
+  get textvalue => null;
+
+  set textValue(String textValue) {}
+  void change(bool value){
+    if(switchButton == true)
+    {
+      setState(() {
+        switchButton = false;
+        textValue = 'off';
+        print('off');
+      });
+    }
+    else
+    {
+      setState(() {
+        switchButton = true;
+         textValue = 'on';
+        print('on');
+      });
+    }
+  }
+
+
+
+  @override
+  Widget build(BuildContext context)
+      {
+    return Scaffold(
+      backgroundColor: Colors.grey.shade200,
+        body: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Container(
+          height: 70.0,
+          width: 350.0,
+          decoration: BoxDecoration(
+           border: Border.all()
+           ),
+
+              child: Column(
+                children: <Widget>[
+                  Padding(
+                    padding: const EdgeInsets.only(right: 0.0),
+                    child: Row(
+                      children: [
+                        Text('Live location sharing',style: TextStyle(fontSize: 17.0),),
+                        Row(
+                          children: <Widget>[
+                                 Padding(
+                                   padding: const EdgeInsets.only(left: 120.0),
+                                   child: Switch(
+                                    onChanged: change,
+                                    value: switchButton,
+                                    activeTrackColor: Colors.black26,
+
+                                  // title (Text('$textvalue', style: TextStyle(fontSize: 5))),
+                            ),
+                                 ),
+                          ],
+                        )
+                      ],
+
+                    ),
+                  ),
+
+                  Padding(
+                    padding: const EdgeInsets.only(left: 270.0),
+                    child: Column(
+                      children: <Widget>[
+                         (Text('$textvalue', style: TextStyle(fontSize: 15))),
+                      ],
+                    ),
+                  )
+
+                ],
+
+
+              ),
+         ),
+        ),
+      );
+  }
+
+}
+
+class Sharedwithme extends StatefulWidget{
+const Sharedwithme({Key? key}) : super(key: key);
+
+@override
+State<StatefulWidget> createState() {
+  return SharedwithmeState();
+}
+}
+
+class SharedwithmeState extends State<Sharedwithme>{
+
+  @override
+  Widget build(BuildContext context)
+  {
+    return Scaffold(
+      backgroundColor: Colors.grey.shade200,
+        body: Center(
+            child: Column(
+              // mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Padding(
+                  padding: const EdgeInsets.all(100.0),
+                  child: Container(
+                    height: 80.0,
+                    width: 80.0,
+                    child: Image.asset("assets/images/sharedwithme.jpeg"),// Didn't found the exact image, replaced with other image
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(top: 10.0,bottom: 0.0),
+                  child: Text('All live location and rides\nshared with you will show',style: TextStyle(color: Colors.black,fontSize: 20.0),),
+                ),
+                Text('up here',style: TextStyle(color: Colors.black,fontSize: 20.0),),
+
+              ],
+            )
+        )
+    );
+  }
+
 }
--- lib/myProfile.dart
+++ lib/myProfile.dart
@@ -1,5 +1,6 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-
+import 'package:percent_indicator/linear_percent_indicator.dart';
 void main() => runApp(const MyProfile());
 
 class MyProfile extends StatelessWidget{
@@ -22,7 +23,407 @@ class MyProfileState extends StatefulWidget{
 class MyProfilePage extends State<MyProfileState>{
   Widget build(BuildContext context){
     return Scaffold(
-      body: Center(child: Text('My profile')),
-    );
+      backgroundColor: Colors.grey.shade200,
+      appBar: AppBar(
+        iconTheme: IconThemeData(color: Colors.black),
+        leading: Padding(
+            padding: const EdgeInsets.all(1.0),
+            child: Builder(
+              builder: (context) =>
+                  IconButton(
+                    icon: Icon(Icons.arrow_back,size: 30.0,),
+                    onPressed: () => Scaffold.of(context).openDrawer(),
+                  ),
+            )
+        ),
+        elevation: 1.0,
+        backgroundColor: Colors.grey.shade200,
+        title: Text("My profile",style: TextStyle(color: Colors.black,fontWeight: FontWeight.bold,fontSize: 19.0),),
+      ),
+
+        body: ListView(
+
+        children: <Widget>[
+
+         Container(
+           decoration: BoxDecoration(
+             border: Border(top: BorderSide(color: Colors.grey.shade300)),
+           ),
+           child: Column(
+             children: <Widget>[
+               Padding(
+                  padding: const EdgeInsets.all(30.0),
+                  child: CircleAvatar(
+                   backgroundImage: AssetImage("assets/images/heart.jpg"),
+                    radius: 80.0,
+               ),
+                ),
+
+                  Text('test1234',style: TextStyle(fontSize: 15.0,fontWeight: FontWeight.bold),),
+
+             ],
+             ),
+         ),
+
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Container(
+              height: 180.0,
+              width: 50.0,
+              
+              decoration: BoxDecoration(
+              border: Border.all(),
+              shape: BoxShape.rectangle,
+              ),
+
+            padding: EdgeInsets.all(0.0),
+             child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+               children: <Widget>[
+                 Padding(
+                   padding: const EdgeInsets.only(left: 15.0),
+                   child: Text('Profile completeness'),
+                 ),
+                 Padding(
+                   padding: const EdgeInsets.all(8.0),
+                   child: Row(
+                     children: <Widget>[
+                     new LinearPercentIndicator(
+                       width: 260.0,
+                       lineHeight: 5.0,
+                       percent: 0.61,
+                       backgroundColor: Colors.grey,
+                       progressColor: Colors.red,
+
+                       trailing: Icon(Icons.check_circle_outlined),
+
+                     ),
+                      Padding(
+                        padding: const EdgeInsets.all(5.0),
+                        child: Text('61%',style: TextStyle(fontSize: 15.0,fontWeight: FontWeight.bold),),
+                      ),
+
+
+                   ]
+                     ),
+                    ),
+                      Column(
+                        children: [
+                          Column(
+                          children: <Widget>[
+                             Padding(
+                               padding: const EdgeInsets.only(left: 0.0,right: 180.0),
+                               child: Text('Last updated on 21 Sept 2021',style: TextStyle(fontSize: 10.0),),
+                             ),
+
+                          ],
+                           ),
+                          Column(
+                            children: [
+                              Padding(
+                                padding: const EdgeInsets.all(8.0),
+                                child: Row(
+                                  children: [
+                                    Icon(
+                                      Icons.email_rounded,
+                                      color: Colors.blue,
+                                    ),
+                                    Padding(
+                                      padding: const EdgeInsets.all(8.0),
+                                      child: Text('Verify email to login using email address',style: TextStyle(fontSize: 12.0),),
+                                    ),
+                                  ],
+                                ),
+
+                              )
+                            ],
+
+                          ),
+                          Column(
+                            children: <Widget>[
+
+                              Padding(
+                                padding: const EdgeInsets.only(right: 60.0),
+                                child: Container(
+                                  height: 35.0,
+                                  width: 200.0,
+
+                                  child: FlatButton( child: Text("Send verification link",style: TextStyle(color: Colors.white),),
+                                  shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(10.0),
+                                  ),
+                                    color: Colors.blue,
+                                    onPressed: () {},
+                                  ),
+
+                                    ),
+                              )
+                            ],
+                          )
+                                ],
+                              ),
+                    ]
+                     ),
+
+                  ),
+                  ),
+
+
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Container(
+                        height: 120.0,
+                        width: 50.0,
+                    decoration: BoxDecoration(
+                    border: Border.all(),
+                    shape: BoxShape.rectangle,
+                    ),
+
+                      child: Column(
+                        children: <Widget>[
+                          Padding(
+                            padding: const EdgeInsets.only(left:0.0,right: 270.0),
+                            child: Text("Personal details",style:TextStyle(fontSize: 10.0,fontWeight: FontWeight.bold)),
+                          ),
+                          Row(
+                              children:<Widget>[
+                                Padding(
+                                  padding: const EdgeInsets.only(left: 0.0,right: 60.0),
+                                  child: Text("Name",style: TextStyle(fontSize: 10.0),),
+                                ),
+                                Row(
+                                  children: <Widget>[
+                                    Padding(
+                                      padding: const EdgeInsets.only(top:10,bottom:10.0,right: 5.0,left: 32.0),
+                                      child: Text("test",style: TextStyle(fontSize: 10.0,fontWeight: FontWeight.bold),),
+                                    )
+                                  ],
+                                ),
+
+                              ]
+
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.only(right: 50.0),
+                            child: Row(
+                              children: <Widget>[
+                                Text("Email address",style: TextStyle(fontSize: 10.0),),
+                                Padding(
+                                  padding: const EdgeInsets.only(right: 8.0,left: 55.0),
+                                  child: Row(
+                                    children: [
+                                      Text("test1234@gmail.com",style: TextStyle(fontSize: 10.0,fontWeight: FontWeight.bold),),
+                                    ],
+                                  ),
+                                )
+                              ],
+                            ),
+                          ),
+                          Row(
+                            children: <Widget>[
+                              Text("Mobile Number",style: TextStyle(fontSize: 10.0),),
+                              Padding(
+                                padding: const EdgeInsets.only(top:10,bottom:10.0,right: 8.0,left: 48.0),
+                                child: Row(
+                                  children: [
+                                    Text("+917795368246",style: TextStyle(fontSize: 10.0,fontWeight: FontWeight.bold),),
+                                  ],
+                                ),
+                              )
+                            ],
+                          ),
+
+                          Row(
+                            children: <Widget>[
+                              Text("Country",style: TextStyle(fontSize: 10.0),),
+                              Padding(
+                                padding: const EdgeInsets.only(right: 8.0,left: 82.0),
+                                child: Row(
+                                  children: [
+                                    Text("India",style: TextStyle(fontSize: 10.0,fontWeight: FontWeight.bold),),
+                                  ],
+                                ),
+                              )
+                            ],
+                          ),
+
+                        ],
+
+
+
+                      ),
+
+
+                    ),
+                  ),
+
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Container(
+                      height: 120.0,
+                      width: 50.0,
+                      decoration: BoxDecoration(
+                        border: Border.all(),
+                        shape: BoxShape.rectangle,
+                      ),
+                    child: Column(
+                      children: <Widget>[
+                        Padding(
+                          padding: const EdgeInsets.only(right: 200.0),
+                          child: Text("Medical insurance information",style:TextStyle(fontSize: 10.0,fontWeight: FontWeight.bold)),
+                        ),
+                        Row(
+                          children:<Widget>[
+                            Padding(
+                              padding: const EdgeInsets.only(left: 0.0,right: 60.0),
+                              child: Text("Blood Group",style: TextStyle(fontSize: 10.0),),
+                            ),
+                            Row(
+                              children: <Widget>[
+                                Padding(
+                                  padding: const EdgeInsets.only(top:10,bottom:10.0,right: 8.0),
+                                  child: Text("B+",style: TextStyle(fontSize: 10.0,fontWeight: FontWeight.bold),),
+                                )
+                              ],
+                            ),
+
+                          ]
+
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.only(right: 50.0),
+                          child: Row(
+                            children: <Widget>[
+                              Text("Health Insurance",style: TextStyle(fontSize: 10.0),),
+                            Padding(
+                              padding: const EdgeInsets.only(right: 8.0,left: 40.0),
+                              child: Row(
+                                children: [
+                                  Text("Bajaj Allianz life Insurance",style: TextStyle(fontSize: 10.0,fontWeight: FontWeight.bold),),
+                                ],
+                              ),
+                            )
+                            ],
+                          ),
+                        ),
+                        Row(
+                          children: <Widget>[
+                            Text("Policy Number",style: TextStyle(fontSize: 10.0),),
+                            Padding(
+                              padding: const EdgeInsets.only(top:10,bottom:10.0,right: 8.0,left: 50.0),
+                              child: Row(
+                                children: [
+                                  Text("5787543",style: TextStyle(fontSize: 10.0,fontWeight: FontWeight.bold),),
+                                ],
+                              ),
+                            )
+                          ],
+                        ),
+
+                        Row(
+                          children: <Widget>[
+                            Text("Health Insurance Card \nNumber",style: TextStyle(fontSize: 10.0),),
+                            Padding(
+                              padding: const EdgeInsets.only(top:0.0,bottom:15.0,right: 8.0,left: 13.0),
+                              child: Row(
+                                children: [
+                                  Text("6fvu5cucyc",style: TextStyle(fontSize: 10.0,fontWeight: FontWeight.bold),),
+                                ],
+                              ),
+                            )
+                          ],
+                        ),
+
+                      ],
+
+
+
+                    ),
+
+
+
+
+                  ),
+                ),
+
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Container(
+                    height: 45.0,
+                    width: 40.0,
+                      decoration: BoxDecoration(
+                        border: Border.all(),
+                        shape: BoxShape.rectangle,
+                      ),
+                    child: Column(
+                      children: <Widget>[
+                        Padding(
+                          padding: const EdgeInsets.only(right: 200.0),
+                          child: Text("Vehicle Insurance information",style:TextStyle(fontSize: 10.0,fontWeight: FontWeight.bold)),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.only(right: 75.0),
+                          child: Text("Edit and view vehicle insurance details in vehicle settings.",style: TextStyle(fontSize: 10.0),),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.only(right: 180.0),
+                          child: Text("Menu > Settings > Vehicle settings",style: TextStyle(fontSize: 10.0,fontWeight: FontWeight.bold),),
+                        )
+                      ],
+                    ),
+                  ),
+                ),
+
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Container(
+                      height: 45.0,
+                      width: 40.0,
+                      decoration: BoxDecoration(
+                        border: Border.all(),
+                        shape: BoxShape.rectangle,
+                      ),
+                     child: Column(
+                       children: <Widget>[
+                         Padding(
+                           padding: const EdgeInsets.only(right: 250.0),
+                           child: Text("Emergency contacts",style: TextStyle(fontSize: 10.0,fontWeight: FontWeight.bold),),
+                         ),
+                         Padding(
+                           padding: const EdgeInsets.only(right: 90.0),
+                           child: Text("Add and edit emergency contact list in vehicle settings.",style: TextStyle(fontSize: 10.0),),
+                         ),
+                          Padding(
+                            padding: const EdgeInsets.only(right: 180.0),
+                            child: Text("Menu > Settings > Vehicle settings",style: TextStyle(fontSize: 10.0,fontWeight: FontWeight.bold),),
+                          )
+                       ],
+                     ),
+
+                      //child: Text(),
+
+
+
+
+                  ),
+                ),
+
+
+
+
+
+
+    ]
+        )
+                  );
+
+
+
+
+
+
+
+
+
   }
 }
